cb7ff1755e9a50cccfd49691eed5a8a74cf6e663
controladoriacharque500 <cont.cha500@gmail.com>
2025-10-15 15:43:51 -0300

    Add files via upload

---
+++ AnimalSoundsApp.py
@@ -0,0 +1,37 @@
+import kivy
+from kivy.app import App
+from kivy.uix.gridlayout import GridLayout
+from kivy.uix.button import Button
+from kivy.core.audio import SoundLoader
+
+kivy.require('2.1.0') # Versão do Kivy que estamos usando
+
+class AnimalButton(Button):
+    def __init__(self, animal_name, **kwargs):
+        super(AnimalButton, self).__init__(**kwargs)
+        self.animal_name = animal_name
+        self.background_normal = f'assets/images/{self.animal_name}.png'
+        self.background_down = f'assets/images/{self.animal_name}.png' # Mantém a mesma imagem ao pressionar
+        self.sound = SoundLoader.load(f'assets/sounds/{self.animal_name}.wav')
+        self.bind(on_press=self.play_sound)
+
+    def play_sound(self, instance):
+        if self.sound:
+            self.sound.play()
+
+class AnimalSoundsApp(App):
+    def build(self):
+        # Layout em grade com 3 colunas
+        layout = GridLayout(cols=3, spacing=10, padding=10)
+
+        # Lista de animais (o nome do arquivo de imagem e som deve ser o mesmo)
+        animais = ['cao', 'gato', 'leao', 'elefante', 'macaco', 'pato']
+
+        for animal in animais:
+            btn = AnimalButton(animal_name=animal)
+            layout.add_widget(btn)
+
+        return layout
+
+if __name__ == '__main__':
+    AnimalSoundsApp().run()
---
+++ buildozer.spec
@@ -0,0 +1,27 @@
+[app]
+title = Sons de Animais
+package.name = sonsdeanimais
+package.domain = org.sonsdeanimais_ByAdriano
+source.dir = .
+source.include_exts = py,png,jpg,kv,atlas
+version = 0.1
+orientation = portrait
+
+# Requisitos para a compilação (simples e estável)
+requirements = python3,kivy,sdl2
+
+# Configurações do Android
+fullscreen = 0
+android.api = 27
+android.minapi = 21
+android.ndk = 23b
+android.archs = arm64-v8a, armeabi-v7a
+android.allow_backup = True
+# android.accept_sdk_license = True  # Pode ser útil se houver problema de licença
+
+# Configurações do P4A
+p4a.extra_args = --branch master
+
+[buildozer]
+log_level = 2
+warn_on_root = 0
